4f2ab91781a501107a8c2fe471a93f10ad6bded6
ConstantineTU <8kozmo8@gmail.com>
2021-08-20 14:09:09 +0500

    init: create task drum-kit

---
+++ drum-kit/js/app.js
@@ -0,0 +1,15 @@
+
+function ibg() {
+	if (isIE()) {
+		let ibg = document.querySelectorAll("._ibg");
+		for (var i = 0; i < ibg.length; i++) {
+			if (ibg[i].querySelector('img') && ibg[i].querySelector('img').getAttribute('src') != null) {
+				ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+			}
+		}
+	}
+}
+ibg();
+
+
+
